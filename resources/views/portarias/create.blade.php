6797658419a234f79cc89727f41b0d5681a25878
Guilherme Frias <guilherme.frias.o.t3@gmail.com>
2026-03-25 16:02:08 -0300

    [feat]: FrontEnd create iniciado sem aprimoramento

--- resources/views/portarias/create.blade.php
+++ resources/views/portarias/create.blade.php
@@ -2,13 +2,24 @@
 
 @section('content')
     <div class="portaria_form_block">
-        <h2 class="portaria_form_title">Crie uma portaria</h2>
+        <div class="col-md-12">
+            <h2 class="portaria_form_title">Crie uma portaria</h2>
+        </div>
         <form class="portaria_form" method="POST" action="/store">
             @csrf
-            <input type="text" name="title">
-            <textarea name="introduction"></textarea>
-            <textarea name="content"></textarea>
-            <button type="submit">Criar Portaria</button>
+            <div>
+                <label class="form-label" for="title">Título:</label>
+                <input class="form-control" type="text" name="title">
+            </div>
+            <div>
+                <label for="introduction">Preâmbulo</label>
+                <textarea class="form-control" name="introduction"></textarea>
+            </div>
+            <div>
+                <label for="content">Conteúdo</label>
+                <textarea class="form-control" name="content"></textarea>
+            </div>
+            <button type="submit" class="btn btn-success">Criar Portaria</button>
         </form>
     </div>
 @endsection('content')
